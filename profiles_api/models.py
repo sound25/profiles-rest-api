e48e53135159152b6a27bf0310e41aafe71a9ca8
Soundarya <t.soundarya25@gmail.com>
2021-10-22 14:12:13 +0530

    added user profile API

--- profiles_api/models.py
+++ profiles_api/models.py
@@ -1,5 +1,5 @@
 from django.db import models
-
+from django.conf import settings
 from django.contrib.auth.models import AbstractBaseUser
 from django.contrib.auth.models import PermissionsMixin
 from django.contrib.auth. models import BaseUserManager
@@ -47,3 +47,20 @@ class UserProfile(AbstractBaseUser,PermissionsMixin):
 
     def __str__(self):
         return self.email
+
+
+class ProfileFeedItem(models.Model):
+    '''Profile status update'''
+
+    user_profile=models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE,
+        
+    )
+    status_text=models.CharField(max_length=255)
+    created_on=models.DateTimeField(auto_now_add=True)
+
+
+    def __str__(self):
+        return self.status_text
+
